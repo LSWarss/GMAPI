1850fd59b601bbd84f2c99537b3311a77f3e35a6
LSWarss <ls.warss98@gmail.com>
2021-02-08 20:13:46 +0100

    Corrected algorithm and with this ended scraping, it could be usefull to scrap every page with 5 s delay to make all request go

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ## To-Implement
 
-- [ ] WebScraping data of premiers and games
+- [x] WebScraping data of premiers and games
 - [ ] Saving the data periodicaly to the database
 - [ ] Create REST API that would take data from the database
 - [ ] Deploy API to some cloud hosting 
--- Scraping/scrape.js
+++ Scraping/scrape.js
@@ -8,18 +8,14 @@ const fs = require('fs')
 const uriPC = 'https://www.metacritic.com/browse/games/release-date/available/pc/date'
 const uriPS5 = 'https://www.metacritic.com/browse/games/release-date/new-releases/ps5/date'
 
-function scrapeGames(url, filename, $) {
+function scrapeGames(url, writeStream) {
 
-    const writeStream = fs.createWriteStream(filename + '.csv', {flags: 'a'})
+   
 
-    // Write Headers
-    writeStream.write('Title, Platform, Date \n');
+    request(url, (error, response, html) => {
+        if (!error && response.statusCode == 200) {
 
-
-            // const gameContent = $('.clamp-summary-wrap');
-            // // console.log(gameContent.html());
-            // const title = gameContent.find('h3').parent().text()
-            // console.log(title)
+            const $ = cheerio.load(html);
 
             $('.clamp-summary-wrap').each((i, el) => {
                 const gameTitle = $(el).find('h3').first().text()
@@ -32,39 +28,55 @@ function scrapeGames(url, filename, $) {
 
                 // console.log(`Game: ${gameTitle}, for ${gamePlatform}, realease at ${gameRelease}`)
             })
-
-            console.log('Scraping done 🤖');
+        } else { 
+        console.log(`There was an expected error ❌: ${error}`)
+        }
+        
+        console.log('Scraping done 🤖');
+    });
 }
 
 function scrapAllPages(url, dataName) {
 
     let path = `.${dataName}.csv`
-    if(fs.existsSync(path)){
+    if (fs.existsSync(path)) {
         fs.unlinkSync(path)
     }
-    
+
+    const writeStream = fs.createWriteStream(dataName + '.csv', { flags: 'a' })
+
+    // Write Headers
+    writeStream.write('Title, Platform, Date \n');
 
     request(url, (error, response, html) => {
-        if(!error && response.statusCode == 200) {
+        if (!error && response.statusCode == 200) { 
             const $ = cheerio.load(html);
-
             const lastPageNumber = $('.last_page').find('a').text()
             console.log(lastPageNumber);
-            if(lastPageNumber === ''){
-                scrapeGames(url, dataName, $);
+
+
+            if (lastPageNumber === '') {
+                scrapeGames(url, writeStream);
             } else {
-                for(let page = 0; page < parseInt(lastPageNumber); page++) {
-                        if(page === 0) {
-                            scrapeGames(url,dataName,$)
-                        } else {
-                            let scrapUrl = url + `?page=${page}`
-                            scrapeGames(scrapUrl,dataName,$)
-                        }
+                for (let page = 0; page < parseInt(lastPageNumber); page++) {
+                    if (page === 0) {
+                        scrapeGames(url, writeStream)
+                    } else {
+                        let scrapUrl = url + `?page=${page}`
+                        scrapeGames(scrapUrl, writeStream)
+                    }
                 }
             }
-            
+
+        } else { 
+            console.log(`There was an expected error ❌: ${error}`)
         }
+
+        
     });
+
+    
+
 }
 
 
