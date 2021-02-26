e5e9ed6b2bb5a6353202763d2b7f6c48ceed3fcd
LSWarss <ls.warss98@gmail.com>
2021-02-26 22:31:30 +0100

    Start of the implementation

--- API/index.js
+++ API/index.js
@@ -1,9 +1,27 @@
+// Dependencies
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const passport = require('passport')
+
+// Routes dependecies
 const gamesController = require('./controller/gamesController')
 const scraperController = require('./controller/scraperController')
+
 const PORT = process.env.PORT || 3000
+
+// definining the app
+const app = express()
+
+// adding Helmet to enhance your API's security 
+app.use(helmet());
+// enables CORS for all requests
+app.use(cors());
+// Logging http requests 
+app.use(morgan('combined'));
+
 
 app.use(bodyParser.json())
 app.use(
