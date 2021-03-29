1e874e7ce6a36d47527956223916664793c383aa
LSWarss <ls.warss98@gmail.com>
2021-03-29 17:27:02 +0200

    Added new scraping method with extras, and deleted redundant cron job

--- Scraping/cron.js
+++ Scraping/cron.js
@@ -8,11 +8,3 @@ cron.schedule('0 */6 * * *', function(){
     console.log("🤖 Running scrapping")
     scrapAllUrls()
 });
-
-cron.schedule('*/5 * * * *', function(){
-    console.log("A five minute cron job for testing etc: ✔️")
-    request('https://api.chucknorris.io/jokes/random', { json: true }, (err, res, body) => {
-        if (err) { return console.log(err); }
-        console.log(body.value);
-    });
-})
--- Scraping/scrape.js
+++ Scraping/scrape.js
@@ -11,6 +11,7 @@ const {
     convertToPgDate
 } = require('./utilities')
 
+
 const metaUrls = {
     uriPC: 'https://www.metacritic.com/browse/games/release-date/coming-soon/pc/date',
     uriPS5: 'https://www.metacritic.com/browse/games/release-date/coming-soon/ps5/date',
@@ -24,34 +25,98 @@ const metaUrls = {
 
 function scrapeGames(url) {
     return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                    request(url, (error, response, html) => {
-                            if (!error && response.statusCode == 200) {
+        setTimeout(() => {
+            request(url, (error, response, html) => {
+                if (!error && response.statusCode == 200) {
 
-                                const $ = cheerio.load(html);
+                    const $ = cheerio.load(html);
 
-                                if ($.length) {
-                                    $('.clamp-summary-wrap').each((i, el) => {
-                                            const gameTitle = $(el).find('h3').first().text()
-                                            const gameDetails = $(el).find('.clamp-details')
-                                            const gamePlatform = gameDetails.find('span').next('span').text().replace(/\s\s+/g, '')
-                                            const gameRelease = gameDetails.find('span').last().text().replace(/\s\s+/g, '')
+                    if ($.length) {
+                        $('.clamp-summary-wrap').each((i, el) => {
+                            const gameTitle = $(el).find('h3').first().text()
+                            const gameDetails = $(el).find('.clamp-details')
+                            const gamePlatform = gameDetails.find('span').next('span').text().replace(/\s\s+/g, '')
+                            const gameRelease = gameDetails.find('span').last().text().replace(/\s\s+/g, '')
 
-                                            // Write row to CSV
-                                            pool.query('INSERT INTO games (title, platform, release_date) VALUES ($1,$2,$3) ON CONFLICT ON CONSTRAINT games_title_key DO NOTHING;', [gameTitle, gamePlatform, convertToPgDate(gameRelease)], (error, results) => {
-                                                if (error) {
-                                                    throw new Error(`Error on inserting to database occured ❌: ${error}`);
-                                                }
-                                            });
-                                        })
-                                console.log(`Scraping done for URL ✅: ${url}`);
-                                resolve();
-                            } else {
-                                reject(`There was an expected error ❌: ${error} and code: ${response.statusCode} on URL: ${url}`)
-                            }
-                        }
-                    })
-            }, 5000)
+                            // Write row to CSV
+                            pool.query('INSERT INTO games (title, platform, release_date) VALUES ($1,$2,$3) ON CONFLICT ON CONSTRAINT games_title_key DO NOTHING;', [gameTitle, gamePlatform, convertToPgDate(gameRelease)], (error, results) => {
+                                if (error) {
+                                    throw new Error(`Error on inserting to database occured ❌: ${error}`);
+                                }
+                            });
+                        })
+                        console.log(`Scraping done for URL ✅: ${url}`);
+                        resolve();
+                    } else {
+                        reject(`There was an expected error ❌: ${error} and code: ${response.statusCode} on URL: ${url}`)
+                    }
+                }
+            })
+        }, 5000)
+    })
+}
+
+function scrapGamesWithExtras(url) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            request(url, (error, response, html) => {
+                if (!error && response.statusCode == 200) {
+                    let $ = cheerio.load(html)
+
+                    if ($.length) {
+                        $('.clamp-summary-wrap').each((i, el) => {
+                            const gameTitle = $(el).find('h3').first().text()
+                            const gameLink = $(el).find('a.title').first().attr('href')
+                            const gameDetails = $(el).find('.clamp-details')
+                            const gamePlatform = gameDetails.find('span').next('span').text().replace(/\s\s+/g, '')
+                            const gameRelease = gameDetails.find('span').last().text().replace(/\s\s+/g, '')
+                            const gameSummary = $(el).find('div.summary').first().text().replace(/\s\s+/g, '')
+                            scrapGameDetails("https://www.metacritic.com" + gameLink).then((response) => {
+                                const gameExtras = response
+                                console.log(response)
+                            })
+                            
+                            // Write row to CSV
+                            pool.query('INSERT INTO games (title, platform, release_date) VALUES ($1,$2,$3) ON CONFLICT ON CONSTRAINT games_title_key DO NOTHING;', [gameTitle, gamePlatform, convertToPgDate(gameRelease)], (error, results) => {
+                                if (error) {
+                                    throw new Error(`Error on inserting to database occured ❌: ${error}`);
+                                }
+                            });
+                        })
+                        console.log(`Scraping done for URL ✅: ${url}`);
+                        resolve();
+                    } else {
+                        reject(`There was an expected error ❌: ${error} and code: ${response.statusCode} on URL: ${url}`)
+                    }
+                }
+            })
+        }, 10000)
+    })
+}
+
+function scrapGameDetails(url) { 
+    return new Promise((resolve, reject) => {
+            request(url, (error, response, html) => {
+                if (!error && response.statusCode == 200) {
+                    let $ = cheerio.load(html)
+                    let gameGenre = ""
+                    let gameDeveloper = ""
+
+                    if ($.length) { 
+                        
+                        
+                        $('div.side_details').each((i, el) => {
+                            const gameDeveloperLi = $(el).find('li.developer')
+                            gameDeveloper = gameDeveloperLi.find('span.data').text().replace(/\s\s+/g, '')
+                            const gameGenreLi = $(el).find('li.product_genre')
+                            gameGenre = gameGenreLi.find('span.data').first().text()
+                        })
+                    }    
+                    resolve([gameGenre, gameDeveloper])
+                } else {
+                    reject(`There was an expected error ❌: ${error} and code: ${response.statusCode} on URL: ${url}`)
+                }
+            })
     })
 }
 
@@ -85,12 +150,17 @@ async function scrapAllPages(url) {
     });
 }
 
+
+
+
 function scrapAllUrls() {
     for (const key in metaUrls) {
         console.log(metaUrls[key])
         scrapAllPages(metaUrls[key])
     }
 }
+
+scrapGamesWithExtras("https://www.metacritic.com/browse/games/release-date/coming-soon/ps5/date")
 
 module.exports = {
     scrapAllUrls
