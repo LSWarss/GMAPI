27295fba0f24e13e39c475fa20f4ef2bb3afab26
LSWarss <ls.warss98@gmail.com>
2021-03-30 08:18:30 +0200

    Fallow up to the quick fix

--- API/controller/scraperController.js
+++ API/controller/scraperController.js
@@ -2,22 +2,8 @@ const { scraper } = require('../../Scraping/scrape')
 const { pool } = require("../model/db")
 
 const startScraperManually = (request, response) => {
-
-    const { scrapToken } = request.body
-
-    if(scrapToken === process.env.SCRAP_TOKEN) {
-        scraper()
-        pool.query('SELECT * FROM games ORDER BY release_date ASC', (error, results) => {
-            if (error) {
-                throw error
-            }
-        })
+    scraper()
         response.status(202).json({ message: 'Started manual scraping'})
-    } else {
-        response.status(400).json({ message: 'Wrong scrap token'})
-    }
-
-  
 }
 
 
