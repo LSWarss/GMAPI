4f49f4502ab7fedc469e87eadd9a6a29f4628798
LSWarss <ls.warss98@gmail.com>
2021-03-30 17:08:29 +0200

    ScraperController implementation correction

--- API/controller/scraperController.js
+++ API/controller/scraperController.js
@@ -1,8 +1,16 @@
 const { scraper } = require('../../Scraping/scrape')
 
 const startScraperManually = (request, response) => {
-    scraper()
-    response.status(202).json({ message: 'Started manual scraping'})
+
+    const { scrapToken } = request.body
+
+    if(scrapToken === process.env.SCRAP_TOKEN) {
+        scraper()
+        response.status(202).json({ message: 'Started manual scraping'})
+    } else {
+        response.status(400).json({ message: 'Wrong scrap token'})
+    }
+    
 }
 
 
