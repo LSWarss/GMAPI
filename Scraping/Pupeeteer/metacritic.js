b9c1b6ed03d091336527d35d27870dd57b802445
LSWarss <ls.warss98@gmail.com>
2021-04-03 14:10:07 +0200

    Correction to build

--- Scraping/Pupeeteer/metacritic.js
+++ Scraping/Pupeeteer/metacritic.js
@@ -14,7 +14,7 @@ const {
 async function scrapePage(url) { 
     try { 
         var t0 = performance.now()
-        var browser = await puppeteer.launch({ headless: true})
+        var browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] })
 
         var page = await browser.newPage()
 
@@ -108,7 +108,7 @@ async function getAllPagesForScraping(baseUrl) {
     try { 
         var t0 = performance.now()
         var linksArray = []
-        var browser = await puppeteer.launch({ headless: true})
+        var browser = await puppeteer.launch({ headless: true,args: ['--no-sandbox'] })
 
         var page = await browser.newPage()
 
